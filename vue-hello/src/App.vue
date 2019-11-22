4b335ad850111ed964bee79cef6e2a3774579e27
陈瑞福 <410232098@qq.com>
2019-11-22 16:29:45 +0800

    team

---
+++ vue-hello/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+@import './assets/base.css';
+h3{height:32px}
+</style>
